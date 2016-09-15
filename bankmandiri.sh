9cc4b556e18b64c5b8891c6303351862906415da
stephen Lukas Wijaya <m26415068@john.petra.ac.id>
2016-09-15 10:07:32 +0700

    Bank mandiri

--- bankmandiri.sh
+++ bankmandiri.sh
@@ -1,4 +1,4 @@
-l=curl -s http://www.bankmandiri.co.id/ | grep USD -A2 | cut -d">" -f2 | cut -d"<" -f1 | xargs | cut -d" " -f2
+jual=curl -s http://www.bankmandiri.co.id/ | grep USD -A2 | cut -d">" -f2 | cut -d"<" -f1 | xargs | cut -d" " -f2
 beli=curl -s http://www.bankmandiri.co.id/ | grep USD -A2 | cut -d">" -f2 | cut -d"<"
 -f1 | xargs | cut -d" " -f3
 echo "Jual = $jual"
